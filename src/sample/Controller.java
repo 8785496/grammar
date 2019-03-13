b55bf82f7f11366424280c1b3ac6fea64adfec04
German <georg.88@mail.ru>
2019-03-14 00:13:24 +0700

    implement edit menu

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,53 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Controller {
+    @FXML private BorderPane ap;
+    @FXML private TextArea editor;
+
     @FXML
-    private void click() {
-        System.out.println("Click");
+    private void menuClose() {
+        Platform.exit();
+    }
+
+    @FXML
+    private void menuOpenFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Открыть файл");
+        Stage stage = (Stage)ap.getScene().getWindow();
+        File file = fileChooser.showOpenDialog(stage);
+        if (file == null) {
+            System.out.println("file not choose");
+            return;
+        }
+        System.out.println(file.toString());
+    }
+
+    @FXML
+    private void menuCopy() {
+        editor.copy();
+    }
+
+    @FXML
+    private void menuPaste() {
+        editor.paste();
+    }
+
+    @FXML
+    private void menuCut() {
+        editor.cut();
+    }
+
+    @FXML
+    private void menuUndo() {
+        editor.undo();
     }
 }
